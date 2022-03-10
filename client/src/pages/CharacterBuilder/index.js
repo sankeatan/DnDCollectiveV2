8eddf0ed99a16ed3bdd384748dfe39fd17f934a0
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-03-10 14:15:16 -0500

    character builder infrastucture in pages

---
+++ client/src/pages/CharacterBuilder/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import '../CharacterBuilder/style.css';
+
+import Auth from '../../utils/auth';
+
+const CharacterBuilder = () => {
+    return (
+    <>
+    </>
+    );
+};
+
+export default CharacterBuilder;
