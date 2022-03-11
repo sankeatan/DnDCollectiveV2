010934d5458d346f7f57543d55b2be9e861f9049
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-03-11 11:52:16 -0500

    added header and footer components, but the footer isn't compiling

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Landing from './pages/Landing';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 // Construct our main GraphQL API endpoint
@@ -46,7 +42,6 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
           <div className="container">
             <Routes>
               <Route 
@@ -58,24 +53,12 @@ function App() {
                 element={<Home />}
               />
               <Route 
-                path="/login"
-                element={<Login />}
-              />
-              <Route 
-                path="/signup"
-                element={<Signup />}
-              />
-              <Route 
                 path="/me"
                 element={<Profile />}
               />
               <Route 
                 path="/profiles/:username"
                 element={<Profile />}
-              />
-              <Route 
-                path="/thoughts/:thoughtId"
-                element={<SingleThought />}
               />
             </Routes>
           </div>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,9 @@
-import React from 'react';
+/* import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import ThoughtForm from '../components/ThoughtForm';
 import ThoughtList from '../components/ThoughtList';
-
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
 
@@ -64,3 +62,4 @@ const Profile = () => {
 };
 
 export default Profile;
+*/
