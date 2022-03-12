78111458da336aeec1e3897a005bfac74385e5ae
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-03-11 21:00:48 -0500

    fixed Link tag in navbar component

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <>
         <ul className="nav nav-pills">
-        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
-        <li className="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li className="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li className="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li className="nav-item"><Link href="#" className="nav-link active" aria-current="page">Home</Link></li>
+        <li className="nav-item"><Link href="#" className="nav-link">Features</Link></li>
+        <li className="nav-item"><Link href="#" class="nav-link">Pricing</Link></li>
+        <li className="nav-item"><Link href="#" class="nav-link">FAQs</Link></li>
+        <li className="nav-item"><Link href="#" class="nav-link">About</Link></li>
         </ul>
         </>
     )
