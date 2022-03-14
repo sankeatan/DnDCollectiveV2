be721fbf0a6c5afd3a642d6dcc887ed82d7b311b
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-03-14 13:13:42 -0400

    reactifying character builder

---
+++ client/src/components/CharBuildOptionBtns/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+<nav className="navbar navbar-expand-lg-build navbar-light buildnav">
+    <div class="collapse navbar-collapse-build" id="navbarNav">
+    <ul class="navbar-nav">
+        <li class="nav-item-build active">
+        <button class="nav-link" href="#">Race</button>
+        </li>
+        <li class="nav-item-build ">
+        <button class="nav-link" href="#">Class</button>
+        </li>
+        <li class="nav-item-build ">
+        <a class="nav-link disabled" href="#">Ability Scores</a>
+        </li>
+        <li class="nav-item-build ">
+        <a class="nav-link disabled" href="#">Languages</a>
+        </li>
+        <li class="nav-item-build ">
+            <a class="nav-link disabled" href="#">Proficiencies</a>
+        </li>
+        <li class="nav-item-build ">
+            <a class="nav-link disabled" href="#">Feats</a>
+        </li>
+        <li class="nav-item-build ">
+            <a class="nav-link disabled" href="#">Inventory</a>
+        </li>
+    </ul>
+    </div>
+</nav>
