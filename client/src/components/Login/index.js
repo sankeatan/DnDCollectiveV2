8cefc8f406648c72dd63031cc35cd127ece09d8f
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-03-16 17:39:06 -0400

    fixed some Link errors

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -5,33 +5,6 @@ import {Modal, Button} from 'react-bootstrap';
 import { LOGIN } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
-// function MyVerticallyCenteredModal(props) {
-//     return (
-//       <Modal
-//         {...props}
-//         size="lg"
-//         aria-labelledby="contained-modal-title-vcenter"
-//         centered
-//       >
-//         <Modal.Header closeButton>
-//           <Modal.Title id="contained-modal-title-vcenter">
-//             Modal heading
-//           </Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <h4>Centered Modal</h4>
-//           <p>
-//             Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-//             dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-//             consectetur ac, vestibulum at eros.
-//           </p>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button onClick={props.onHide}>Close</Button>
-//         </Modal.Footer>
-//       </Modal>
-//     );
-//   }
 
 function Login(props) {
   const [formState, setFormState] = useState({ username: '', password: '' });
